9143310f7c966e1309f6799a85f6832e5d518a07
Noobdahl <michael.nordahl@hotmail.com>
2022-09-12 15:14:28 +0200

    Update Program.cs

--- GroceryStore/Program.cs
+++ GroceryStore/Program.cs
@@ -12,116 +12,141 @@
 // Tell the user that the product is not available
 // 
 
-List<string> warehouse = new();
-warehouse.Add("Old cheese");
-warehouse.Add("Stanky fish");
-warehouse.Add("Rotten tomato");
-warehouse.Add("Smelly egg");
+//List<string> warehouse = new();
+//warehouse.Add("Old cheese");
+//warehouse.Add("Stanky fish");
+//warehouse.Add("Rotten tomato");
+//warehouse.Add("Smelly egg");
 
-List<string> shoppingCart = new();
-List<string> userInventory = new();
+//List<string> shoppingCart = new();
+//List<string> userInventory = new();
 
-while(true)
-{ 
-    Console.Clear();
-    WelcomeMessage();
+//while(true)
+//{ 
+//    Console.Clear();
+//    WelcomeMessage();
 
-    PrintWarehouse();
-    PrintShoppingcart();
+//    PrintWarehouse();
+//    PrintShoppingcart();
 
-    string userInput = Console.ReadLine();
-    if (userInput == "cart")
-    {
-        PrintShoppingcart();
-    }
-    else if (IsProductAvailable(userInput))
-    {
-        MoveProductToShoppingCart(userInput);
-        Print("\n" + userInput + " was added to your cart.");
-    }
-    else if (IsProductInCart(userInput))
-    {
-        MoveProductFromShoppingCart(userInput);
-        Print("\n" + userInput + " was removed from your cart.");
-    }
-    else
-    {
-        Print("The product you are looking for doesn't exist...");
-    }
+//    string userInput = Console.ReadLine();
+//    if (userInput == "cart")
+//    {
+//        PrintShoppingcart();
+//    }
+//    else if (IsProductAvailable(userInput))
+//    {
+//        MoveProductToShoppingCart(userInput);
+//        Print("\n" + userInput + " was added to your cart.");
+//    }
+//    else if (IsProductInCart(userInput))
+//    {
+//        MoveProductFromShoppingCart(userInput);
+//        Print("\n" + userInput + " was removed from your cart.");
+//    }
+//    else
+//    {
+//        Print("The product you are looking for doesn't exist...");
+//    }
 
-    Console.WriteLine("\nPress enter to continue...");
-    Console.ReadLine();
-}
+//    Console.WriteLine("\nPress enter to continue...");
+//    Console.ReadLine();
+//}
 
-// METHODS
+//// METHODS
 
-void PrintShoppingcart()
-{
-    if (shoppingCart.Count > 0)
-    {
-        Console.SetCursorPosition(0, 15);
-        Print("Here is your current shoppingcart:");
-        foreach (string item in shoppingCart)
-        {
-            Print(item);
-        }
-    }
-    else
-    {
-        Console.SetCursorPosition(0, 15);
-        Print("Your cart is empty...");
-    }
-    Console.WriteLine();
-}
+//void PrintShoppingcart()
+//{
+//    if (shoppingCart.Count > 0)
+//    {
+//        Console.SetCursorPosition(0, 15);
+//        Print("Here is your current shoppingcart:");
+//        foreach (string item in shoppingCart)
+//        {
+//            Print(item);
+//        }
+//    }
+//    else
+//    {
+//        Console.SetCursorPosition(0, 15);
+//        Print("Your cart is empty...");
+//    }
+//    Console.WriteLine();
+//}
 
-void MoveProductToShoppingCart(string product)
-{
-    warehouse.Remove(product);
-    shoppingCart.Add(product);
-}
+//void MoveProductToShoppingCart(string product)
+//{
+//    warehouse.Remove(product);
+//    shoppingCart.Add(product);
+//}
 
-void MoveProductFromShoppingCart(string product)
-{
-    shoppingCart.Remove(product);
-    warehouse.Add(product);
-}
+//void MoveProductFromShoppingCart(string product)
+//{
+//    shoppingCart.Remove(product);
+//    warehouse.Add(product);
+//}
 
-bool IsProductAvailable(string product)
-{
-    foreach (string thing in warehouse)
-    {
-        if (thing == product)
-            return true;
-    }
-    return false;
-}
+//bool IsProductAvailable(string product)
+//{
+//    foreach (string thing in warehouse)
+//    {
+//        if (thing == product)
+//            return true;
+//    }
+//    return false;
+//}
 
-bool IsProductInCart(string product)
-{
-    foreach (string thing in shoppingCart)
-    {
-        if (thing == product)
-            return true;
-    }
-    return false;
-}
+//bool IsProductInCart(string product)
+//{
+//    foreach (string thing in shoppingCart)
+//    {
+//        if (thing == product)
+//            return true;
+//    }
+//    return false;
+//}
 
-void PrintWarehouse()
-{
-    foreach (string ware in warehouse)
-    {
-        Console.WriteLine(" - " + ware);
-    }
-}
+//void PrintWarehouse()
+//{
+//    foreach (string ware in warehouse)
+//    {
+//        Console.WriteLine(" - " + ware);
+//    }
+//}
 
-void Print(string msg)
-{
-    Console.WriteLine(msg);
-}
+//void Print(string msg)
+//{
+//    Console.WriteLine(msg);
+//}
 
-void WelcomeMessage()
-{
-    Print("Welcome to my Gross Store!");
-    Print("Enter 'cart' to view your shoppingcart, or 'Old cheese' to buy an old cheese.");
-    Print("Here's whats in the store:\n");
-}
+//void WelcomeMessage()
+//{
+//    Print("Welcome to my Gross Store!");
+//    Print("Enter 'cart' to view your shoppingcart, or 'Old cheese' to buy an old cheese.");
+//    Print("Here's whats in the store:\n");
+//}
+
+
+
+
+
+// Övning 1
+
+//string firstNumber = Console.ReadLine();
+//string raknesatt = Console.ReadLine();
+//string secondNumber = Console.ReadLine();
+//int svar = 0;
+
+//int firstConvert = Convert.ToInt32(firstNumber);
+//int secondConvert = Convert.ToInt32(secondNumber);
+
+//if (raknesatt == "+")
+//{
+//    svar = firstConvert + secondConvert;
+//}
+//else if (raknesatt == "-")
+//{
+//    svar = firstConvert - secondConvert;
+//}
+
+//Console.WriteLine(firstNumber + raknesatt + secondNumber + "=" + svar);
